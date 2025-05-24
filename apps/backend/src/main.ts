96b1433fa6bbe12b31bedc3f29f2c3e2d20db047
Saad Rawasia <saad.rawasia15@gmail.com>
2025-05-24 22:48:48 +0200

    Implement Basic SQlite3 database

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import db from './db/db';
 
 const app = express();
 const port = 4000;
@@ -8,62 +9,95 @@ const port = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-interface DataItem extends Record<string, unknown> {
+// interface DataItem extends Record<string, unknown> {
+//   id: number;
+// }
+
+// interface DB {
+//   [resource: string]: DataItem[];
+// }
+
+// const db: DB = {
+//   posts: [{ id: 1, title: 'Hello World', body: 'Welcome to the mock API!' }],
+// };
+
+type Record = {
   id: number;
-}
-
-interface DB {
-  [resource: string]: DataItem[];
-}
-
-const db: DB = {
-  posts: [{ id: 1, title: 'Hello World', body: 'Welcome to the mock API!' }],
+  resource_id: number;
+  data: number;
 };
 
-app.post('/define-schema', (req: Request, res: Response) => {
-  const { resource, initialData } = req.body;
-  console.log({ resource });
-  if (!resource) return res.status(400).send('Missing resource name');
-  db[resource] = initialData || [];
-  return res.status(201).json({ message: `Resource '${resource}' created.` });
+app.post('/resource', async (req: Request, res: Response) => {
+  const { resource } = req.body;
+  if (!resource)
+    return res.status(400).json({ message: 'Missing resource name' });
+  const existingResource = await db.get(
+    'Select * from resources where name = ?',
+    [resource]
+  );
+  if (existingResource) {
+    return res
+      .status(400)
+      .json({ message: 'Resource with this name already exist' });
+  }
+
+  const { lastID } = await db.run(
+    'INSERT INTO resources(name, schema_definition) VALUES(?, ?)',
+    [resource, '']
+  );
+
+  return res
+    .status(201)
+    .json({ id: lastID, name: resource, schema_definition: '' });
 });
 
-app.get('/resources', (req: Request, res: Response) => {
-  return res.json({ data: Object.keys(db) });
+app.get('/resources', async (req: Request, res: Response) => {
+  try {
+    const query = 'Select * from resources';
+    const resources = await db.all(query);
+    return res.json(resources);
+  } catch (err) {
+    console.error('DB error:', err);
+    return res.json({ message: 'Resources not found' });
+  }
 });
 
-app.all('/:resource/:id?', (req: Request, res: Response): Response | void => {
-  const { resource, id } = req.params;
+app.all('/:resourceId/:id?', async (req: Request, res: Response) => {
+  const { resource } = req.params;
   const method = req.method;
-
-  if (!db[resource]) return res.status(404).send('Resource not found');
 
   switch (method) {
     case 'GET':
-      return id
-        ? res.json(db[resource].find((item) => item.id === Number(id)))
-        : res.json(db[resource]);
+      try {
+        const query = 'Select * from records where resource_id = ?';
+        const records = (await db.all(query, [resource])) as Record[];
+        const data = records.map((record) => record.data);
+        return res.json(data);
+      } catch (err) {
+        console.error('DB error:', err);
+        return res.json({ message: 'Resources not found' });
+      }
 
-    case 'POST': {
-      const newItem: DataItem = { id: Date.now(), ...req.body };
-      db[resource].push(newItem);
-      return res.status(201).json(newItem);
-    }
+    // case 'POST': {
+    //   const newItem: DataItem = { id: Date.now(), ...req.body };
+    //   db[resource].push(newItem);
+    //   return res.status(201).json(newItem);
+    // }
 
-    case 'PUT':
-    case 'PATCH': {
-      const idx = db[resource].findIndex((item) => item.id === Number(id));
-      if (idx === -1) return res.status(404).send('Not found');
-      db[resource][idx] = { ...db[resource][idx], ...req.body };
-      return res.json(db[resource][idx]);
-    }
+    // case 'PUT':
+    // case 'PATCH': {
+    //   const idx = db[resource].findIndex((item) => item.id === Number(id));
+    //   if (idx === -1) return res.status(404).send('Not found');
+    //   db[resource][idx] = { ...db[resource][idx], ...req.body };
+    //   return res.json(db[resource][idx]);
+    // }
 
-    case 'DELETE':
-      db[resource] = db[resource].filter((item) => item.id !== Number(id));
-      return res.status(204).send();
+    // case 'DELETE':
+    //   db[resource] = db[resource].filter((item) => item.id !== Number(id));
+    //   return res.status(204).send();
 
     default:
-      return res.status(405).send('Method Not Allowed');
+      return res.status(405).json({ message: 'Method Not Allowed' });
   }
 });
 
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -4,20 +4,31 @@ interface DataItem extends Record<string, unknown> {
   id: number;
 }
 
+type Resource = {
+  id: string;
+  name: string;
+  schema_definition: string;
+};
+
+type Message = {
+  message: string;
+};
+
 export default function App() {
-  const [resources, setResources] = useState<string[]>([]);
+  const [resources, setResources] = useState<Resource[]>([]);
   const [data, setData] = useState<DataItem[]>([]);
   const [resourceName, setResourceName] = useState('');
 
   const fetchAllResources = async () => {
     const res = await fetch(`http://localhost:4000/resources`);
-    const json: { data: string[] } = await res.json();
-    setResources(json.data);
+    const json: Resource[] = await res.json();
+    setResources(json);
   };
 
-  const fetchData = async (resource: string) => {
-    const res = await fetch(`http://localhost:4000/${resource}`);
+  const fetchData = async (resourceId: string) => {
+    const res = await fetch(`http://localhost:4000/${resourceId}`);
     const json = await res.json();
+    console.log({ resourceId, json });
     setData(json);
   };
 
@@ -26,14 +37,16 @@ export default function App() {
   }, []);
 
   const createSchema = async () => {
-    const res = await fetch('http://localhost:4000/define-schema', {
+    const res = await fetch('http://localhost:4000/resource', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ resource: resourceName }),
     });
-    const json = await res.json();
-    console.log({ json, resourceName });
-    setResources([...resources, resourceName]);
+    const json: Resource | Message = await res.json();
+    if ('id' in json) {
+      setResources([...resources, json]);
+    }
+
     setResourceName('');
   };
 
@@ -49,8 +62,8 @@ export default function App() {
       <div>
         <h2>Available Resources:</h2>
         {resources.map((r) => (
-          <button key={r} onClick={() => fetchData(r)}>
-            {r}
+          <button key={r.id} onClick={() => fetchData(r.id)}>
+            {r.name}
           </button>
         ))}
       </div>
