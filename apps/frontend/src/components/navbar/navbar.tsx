d6107ae3763ccb4b2f1f4fe4e8e7ad5edf6f8470
Saad Rawasia <saad.rawasia15@gmail.com>
2025-06-19 11:52:06 +0200

    improve-navbar (#42)

--- apps/frontend/src/components/navbar/navbar.tsx
+++ apps/frontend/src/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignOutButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignOutButton, useUser } from '@clerk/clerk-react';
 import { Link, useNavigate } from '@tanstack/react-router';
 
 import Logo from '../logo/logo';
@@ -14,6 +14,9 @@ import { Separator } from '../ui/separator';
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const { user } = useUser();
+  const userInitials = `${user?.firstName?.[0].toUpperCase() ?? ''}${user?.lastName?.[0].toUpperCase() ?? ''}`;
+
   return (
     <nav className="flex justify-between">
       <Logo withText={true} />
@@ -40,11 +43,18 @@ export default function Navbar() {
         <DropdownMenu>
           <DropdownMenuTrigger asChild className="cursor-pointer">
             <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
+              <AvatarImage src="" />
+              <AvatarFallback>{userInitials}</AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="flex flex-col gap-2">
+            <DropdownMenuItem className="cursor-pointer hover:bg-transparent">
+              <Link to="/projects">
+                <Button type="button" variant="link" className="hover:no-underline">
+                  Projects
+                </Button>
+              </Link>
+            </DropdownMenuItem>
             <DropdownMenuItem className="cursor-pointer hover:bg-transparent">
               <Link to="/user-settings">
                 <Button type="button" variant="link" className="hover:no-underline">
